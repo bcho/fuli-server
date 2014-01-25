eaf2d6e2e35725a1dfd60d0fa485b26f775e5b44
hbc <bcxxxxxx@gmail.com>
2014-01-25 21:29:57 +0800

    Update schema.sql base on migrations.

--- configs/schema.sql
+++ configs/schema.sql
@@ -4,20 +4,21 @@ DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `categories`;
 
 CREATE TABLE `categories` (
-	`id` int PRIMARY KEY,
+	`id` int PRIMARY KEY AUTO_INCREMENT,
 	`name` varchar(255),
 	`created_at` timestamp,
 	`updated_at` timestamp
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE `users` (
-	`id` int PRIMARY KEY,
-	`student_id` int
+	`id` int PRIMARY KEY AUTO_INCREMENT,
+        `users` VARCHAR(20) NOT NULL,
+	`student_id` VARCHAR(12) NOT NULL
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE `links` (
-	`id` int PRIMARY KEY,
-	`url` varchar(255),
-	`title` varchar(255),
-	`description` varchar(255),
+	`id` int PRIMARY KEY AUTO_INCREMENT,
+	`url` varchar(5000) NOT NULL,
+	`title` varchar(5000) NOT NULL,
+	`description` varchar(5000) NOT NULL,
 	`category_id` int,
 	`created_at` timestamp,
 	`updated_at` timestamp,
@@ -26,9 +27,10 @@ CREATE TABLE `links` (
 	FOREIGN KEY(`user_id`) REFERENCES `users` (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE `comments` (
-	`id` int PRIMARY KEY,
+	`id` int PRIMARY KEY AUTO_INCREMENT,
 	`link_id` int,
 	`user_id` int,
+        `content` varchar(5000) NOT NULL,
 	`created_at` timestamp,
 	`updated_at` timestamp,
 	FOREIGN KEY(`link_id`) REFERENCES `links` (`id`),
