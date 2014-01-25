eb2686171ae58de082d6fa25fe9a421fafad0e7a
hbc <bcxxxxxx@gmail.com>
2014-01-25 22:23:31 +0800

    Update SAE rewrite rules.
    Move `index.php` to root from `public_html`

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+define('ROOT_PATH', realpath('.'));
+define('VENDOR_PATH', realpath(ROOT_PATH . '/vendor'));
+define('APP_PATH', realpath(ROOT_PATH . '/app'));
+
+require_once VENDOR_PATH . '/autoload.php';
+
+$app = new Slim();
+
+// setup configurations
+require_once APP_PATH . '/configs/app.php';
+
+// setup routes
+require_once APP_PATH . '/routes.php';
+
+$app->log->info('app loaded');
+
+$app->run();
